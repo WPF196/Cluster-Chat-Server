277be21714c32a695dc2fe43b541f5733c7d2c10
WPF196 <1962978635@qq.com>
2023-09-16 18:47:53 +0800

    :sparkles:add class_db

---
+++ src/server/db/db.cpp
@@ -0,0 +1,48 @@
+#include "db.h"
+
+// 数据库配置信息
+static string server = "127.0.0.1";
+static string user = "root";
+static string password = "1962978635";
+static string dbname = "chat";
+
+MySQL::MySQL()
+{
+    _conn = mysql_init(nullptr);
+}
+
+MySQL::~MySQL()
+{
+    if (_conn != nullptr)
+        mysql_close(_conn);
+}
+
+bool MySQL::connect()
+{
+    MYSQL *p = mysql_real_connect(_conn, server.c_str(), user.c_str(),
+    password.c_str(), dbname.c_str(), 3306, nullptr, 0);
+    
+    if (p != nullptr)
+        // 设置中文编码
+        mysql_query(_conn, "set names gbk");
+    
+    return p;
+}
+
+bool MySQL::update(string sql)
+{
+    if (mysql_query(_conn, sql.c_str())){
+        LOG_INFO << __FILE__ << ":" << __LINE__ << ":" << sql << "更新失败!";
+        return false;
+    }
+    return true;
+}
+
+MYSQL_RES* MySQL::query(string sql)
+{
+    if (mysql_query(_conn, sql.c_str())){
+        LOG_INFO << __FILE__ << ":" << __LINE__ << ":" << sql << "查询失败!";
+        return nullptr;
+    }
+    return mysql_use_result(_conn);
+}
