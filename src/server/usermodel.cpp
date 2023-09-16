80e463bb65fc399c4ae4acfc420e23ce93881b6c
WPF196 <1962978635@qq.com>
2023-09-16 20:18:55 +0800

    :tada:add class_user

---
+++ src/server/usermodel.cpp
@@ -0,0 +1,24 @@
+#include "usermodel.hpp"
+#include "db.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool UserModel::insert(User &user)
+{
+    // 1. 组装sql语句
+    char sql[1024] = {0};
+    sprintf(sql, "insert into User(name, password, state) values('%s', '%s', '%s')",
+        user.getName().c_str(), user.getPwd().c_str(), user.getState().c_str());
+
+    MySQL mysql;
+    if(mysql.connect()){
+        if(mysql.update(sql)){
+            // 获取插入成功的用户的数据库id
+            user.setId(mysql_insert_id(mysql.getConnection()));
+            return true;
+        }
+    }
+    return false;
+}
